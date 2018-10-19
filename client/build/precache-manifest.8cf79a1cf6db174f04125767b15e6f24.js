9b9b3349d0bf4c52aca78d7a5244f3293c44cf42
rgaritta <fender8@bellsouth.net>
2018-10-18 20:53:09 -0400

    heroku test

---
+++ client/build/precache-manifest.8cf79a1cf6db174f04125767b15e6f24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e2158ae6d1f4f86eb5e",
+    "url": "/static/js/main.5e2158ae.chunk.js"
+  },
+  {
+    "revision": "643606869291846b6d99",
+    "url": "/static/js/1.64360686.chunk.js"
+  },
+  {
+    "revision": "5e2158ae6d1f4f86eb5e",
+    "url": "/static/css/main.1f074ab4.chunk.css"
+  },
+  {
+    "revision": "7b173a7a71bdf11b1327b277de34bf5e",
+    "url": "/index.html"
+  }
+];
